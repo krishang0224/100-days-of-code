c12434152b87a3d564ea9c8b69298a1fb0818d3f
krishang0224 <krishanghanda@gmail.com>
2026-08-11 19:04:25 +0530

    Day 2: Added circle area and perimeter solutions

---
+++ Day 2/rectangle_area_and_peri.c
@@ -0,0 +1,17 @@
+// Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
+#include <stdio.h>
+
+int main() {
+    float length, breadth;
+
+    printf("Enter length and breadth: ");
+    scanf("%f %f", &length, &breadth);
+    float area = length * breadth;
+    float perimeter = 2 * (length + breadth);
+
+    printf("Area: %f", area);
+    printf("Perimeter: %f", perimeter);
+
+    return 0;
+}

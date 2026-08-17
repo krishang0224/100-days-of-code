64c04e574cd2ac4b0ff3b45a72526c1208819bce
krishang0224 <krishanghanda@gmail.com>
2026-08-17 23:49:35 +0530

    is number odd or even and is number positive,negative or zero

---
+++ Day 6/odd_or_even.c
@@ -0,0 +1,16 @@
+// Write a program to input an integer and check whether it is even or odd using if–else.
+
+
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d",&x);
+    if (x%2 == 0){
+        printf("%d is even\n", x);
+    }
+    else{
+        printf("Number is odd");
+    }
+    return 0;
+}

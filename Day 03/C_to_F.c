a960a8baeac1fbe1b100d46b1ad9270c2f7a0342
krishang0224 <krishanghanda@gmail.com>
2026-08-22 19:38:39 +0530

    Celsius to Fahrenhiet and 2 number swap

--- Day 03/C_to_F.c
+++ Day 03/C_to_F.c
@@ -4,7 +4,7 @@ int main(){
     float C;
     printf("Enter temperature in C: ");
     scanf("%f", &C);
-    float F = C * (9/5) + 32;
-    printf("%f",F);
+    float F = C * (9.0/5.0) + 32;
+    printf("%.2f",F);
     return 0;
 }

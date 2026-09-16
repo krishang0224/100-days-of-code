e2b22686c309a42d9eb4b9de7d9057d0485ac03b
krishang0224 <krishanghanda@gmail.com>
2026-09-16 18:13:54 +0530

    Right-Aligned Number and Vertical Star Patterns

---
+++ Day 26/vertical_star_pattern.c
@@ -0,0 +1,40 @@
+/* Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+* */
+#include <stdio.h>
+
+int main(){
+    for(int i = 1; i <= 5; i += 2){
+        for(int j = 1; j <= i; j++){
+            printf("*\n");
+        }
+        printf("\n");
+    }
+
+    for(int i = 3; i >= 1; i -= 2){
+        for(int j = 1; j <= i; j++){
+            printf("*\n");
+        }
+        if(i != 1){
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
